8918277f8012af92382980b7b52a58fde0c30dcc
Sampath <sampathsamaraweera28@gmail.com>
2024-11-14 13:34:23 +0530

    initial commit

---
+++ src/contexts/Context.js
@@ -0,0 +1,27 @@
+import React, {createContext, useContext, useState} from "react";
+
+const Context = createContext();
+
+export const Provider = ({children}) => {
+    // Add global states here
+
+
+    return(
+        <Context.Provider
+            value={{
+                // return states here
+
+            }}
+        >
+            {children}
+        </Context.Provider>
+    );
+};
+
+export const useCustomContext = () => {
+    const context = useContext(Context);
+    if (!context){
+        throw new Error('useCustomContext must be used within a Provider')
+    }
+    return context;
+}
