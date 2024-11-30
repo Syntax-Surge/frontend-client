bc616cad956a896586836d8a3640de0ddcb3ac36
Yasindu9 <144378880+Yasindu9@users.noreply.github.com>
2024-11-30 23:28:23 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Provider } from './contexts/Context';
-import Home from './pages/Home'; 
-import SignUp from './pages/auth pages/signUp';
-import SignIn from './pages/auth pages/signIn';
-import Shop from './pages/shop/Shop';
-import Item from './pages/item/Item';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "./contexts/Context";
+import Home from "./pages/Home";
+import SignUp from "./pages/auth pages/signUp";
+import SignIn from "./pages/auth pages/signIn";
+import Shop from "./pages/shop/Shop";
+import Item from "./pages/item/Item";
+import CustomerReviews from "./pages/reviews/Reviews";
+import CustomerReview from "./pages/reviews/CustomerReview";
 
 // border border-black
 
@@ -15,14 +17,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route>
-            <Route path='/' element={<Home />} />
-            <Route path='/shop' element={<Shop showHeroAndFilter={true} />} />
-            <Route path='/item/:id' element={<Item />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/shop" element={<Shop showHeroAndFilter={true} />} />
+            <Route path="/item/:id" element={<Item />} />
           </Route>
 
           <Route>
-            <Route path='/auth/signup' element={<SignUp />} />
-            <Route path='/auth/signIn' element={<SignIn />} />
+            <Route path="/auth/signup" element={<SignUp />} />
+            <Route path="/auth/signIn" element={<SignIn />} />
+          </Route>
+
+          <Route>
+            <Route path="/reviews" element={<CustomerReview />} />
           </Route>
         </Routes>
       </BrowserRouter>
