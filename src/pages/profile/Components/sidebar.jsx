83f49910b613debd02a149fb0edecd4c049f9e79
CharuSachindu <charusachindu@gmail.com>
2024-12-03 00:22:11 +0530

    order v0.1

--- src/pages/profile/Components/sidebar.jsx
+++ src/pages/profile/Components/sidebar.jsx
@@ -1,7 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { sidebardata } from './sidebardata'
+import axios from 'axios';
+
+
 
 function Sidebar({ setActiveTab, activeTab }) {
+
+
+
+  const [image, setImage] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const [userId, setUserId] = useState(2);
+  const fileInputRef = useRef(null);
+  const [refreshDetails, setRefreshDetails] = useState(true);
+  const [imageUrlDB, setImageUrlDB] = useState();
+  const [error, setError] = useState("")
+
+
+
+
+
   const HorizontalLine = (val) => {
     if (activeTab === val.link) {
       return (
@@ -10,16 +28,92 @@ function Sidebar({ setActiveTab, activeTab }) {
     }
   };
 
+  const handleFileInput = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('image', file);
+
+      console.log(formData);
+      
+  
+      // Debug: Log FormData contents
+      for (const [key, value] of formData.entries()) {
+        console.log(`${key}:`, value);
+      }
+  
+      try {
+        const uploadResponse = await axios.post(`http://localhost:4000/api/v1/users/uploadProfileImage`, formData, {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        });
+        const imageUrl = uploadResponse.data.url;
+        console.log(imageUrl);
+        
+  
+        await axios.post(`http://localhost:4000/api/v1/users/updateUserProfile?id=${userId}`, { imageUrl });
+        setImageUrl(imageUrl); // Update the state 
+        setRefreshDetails((prev) => !prev);
+      } catch (error) {
+        console.error('Error uploading or updating:', error.response?.data || error.message);
+        if (fileInputRef.current) fileInputRef.current.value = ""; // Reset file input
+      }
+    }
+  };
+  
+
+
+  useEffect(() => {
+    const userData = async () => {
+      //   setLoading(true); // Start loading
+      try {
+        const response = await axios.get(`http://localhost:4000/api/v1/users/getUserByID?id=${userId}`); // Replace with your API endpoint
+        setImageUrlDB(response.data.profileImage);
+
+
+        // console.log(response.data.orderItems);
+
+        // setOrders(response.data.orderItems); // Save response data to state
+        // console.log(orders);
+
+        // console.log(orders);
+
+      } catch (err) {
+        setError(err.message); // Handle errors
+      }
+    };
+
+    userData();
+  }, [refreshDetails]);
+
+  const handleFileInputClick = () => {
+    // Programmatically trigger the file input click
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+
+
+
   return (
     <div>
       <div className=' bg-[#1B786F] ml-[400px] text-center rounded-xl pl-3 pr-6 pt-10 pb-10'>
         <div className='pt-5 pb-5'>
           <div className="flex flex-col items-center">
             <img
-              src="https://via.placeholder.com/100"
+              src={imageUrlDB ? imageUrlDB : "https://via.placeholder.com/100"}
               alt="Profile"
               className="rounded-full w-36 h-36 mb-2"
+              onClick={handleFileInputClick}
             />
+            <input
+              ref={fileInputRef}
+              type="file"
+              id="file-upload"
+              className=" hidden"
+              onChange={handleFileInput}
+            />
+
             <h2 className="mt-2 font-semibold text-3xl text-gray-300">Sofia Havertz</h2>
           </div>
         </div>
--- src/pages/profile/orders.jsx
+++ src/pages/profile/orders.jsx
@@ -1,109 +1,231 @@
-import React from 'react'
-import { Card, Typography } from "@material-tailwind/react";
+import React, { useState, useEffect } from 'react'
+import {
+  Card,
+  Typography,
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import axios from 'axios';
+
+
+
+
+
+
+
+
+
+
+
 
 const Orders = () => {
 
-    const TABLE_HEAD = ["Number ID", "Date", "Status", "Price"];
- 
-const TABLE_ROWS = [
-  {
-    id: "#001",
-    date: "October 17, 2023",
-    status: "Delivered",
-    price: "$345.00",
-  },
-  {
-    id: "#002",
-    date: "October 17, 2023",
-    status: "Delivered",
-    price: "$345.00",
-  },
-  {
-    id: "#003",
-    date: "October 17, 2023",
-    status: "Delivered",
-    price: "$345.00",
-  },
-  {
-    id: "#004",
-    date: "QOctober 17, 2023",
-    status: "Delivered",
-    price: "$345.00",
-  },
-];
+  const TABLE_HEAD = ["Number ID", "Date", "Status", "Price"];
+  const [open, setOpen] = React.useState(false);
+  const [userId, setUserId] = useState(2);
+  const [user, setUser] = useState();
+  const [selectId, setSelectId] = useState();
+  const [refreshDetails, setRefreshDetails] = useState(false);
+  const [error, setError] = useState();
+  const [orders, setOrders] = useState([]);
+  const [selectedData, setSelectedData] = useState([]);
+  const [response, setResponse] = useState([]);
 
+  useEffect(() => {
+    const userData = async () => {
+      //   setLoading(true); // Start loading
+      try {
+        const response = await axios.get(`http://localhost:4000/api/v1/users/getOrders?id=${userId}`); // Replace with your API endpoint 
+        
+        
+        // console.log(response.data.orderItems);
+
+        setOrders(response.data.orderItems); // Save response data to state
+        console.log(orders);
+        
+        // console.log(orders);
+
+      } catch (err) {
+        setError(err.message); // Handle errors
+      }
+    };
+
+    userData();
+  }, [refreshDetails]);
+
+
+
+
+
+
+
+
+  const handleOpen = (orderId,index) => {
+    console.log(orderId);
+    console.log(index); 
+    
+    
+    // const filteredData = orders.filter((item) => item.orderId === orderId);
+    // const filteredData = orders.filter((item) => Number(item[index].items) === Number(orderId));
+    const itemData = orders[index].items;
+console.log(itemData); 
+    // console.log("orders");
+    // console.log(orders);
+    // console.log(filteredData);
+
+    setSelectedData(itemData);
+    setSelectId(orderId);
+    setOpen(!open)
+
+  };
+
+  const groupByOrderId = (data) => {
+    return data.reduce((acc, curr) => {
+      // Ensure we're grouping based on orderId
+      if (!acc[curr.orderId]) {
+        acc[curr.orderId] = []; // Initialize an array for each orderId
+      }
+      acc[curr.orderId].push(curr); // Push the current item into the corresponding group
+      return acc;
+    }, {});
+  };
+
+  const groupedData = groupByOrderId(orders);
 
   return (
     <div>
-        <div>
+      <div>
         <div className=' text-2xl font-bold w-2/4 pb-5 font-sans'>
-                        Order History
-                    </div>
-        <Card className="h-full w-11/12  px-6 mr-10">
-      <table className="w-full min-w-max table-auto text-left">
-        <thead>
-          <tr>
-            {TABLE_HEAD.map((head) => (
-              <th key={head} className="border-b border-gray-300 pb-4 pt-10">
-                <Typography
-                  variant="small"
-                  color="blue-gray"
-                  className="font-bold leading-none"
-                >
-                  {head}
-                </Typography>
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {TABLE_ROWS.map(({ id, date, status, price }, index) => {
-            const isLast = index === TABLE_ROWS.length - 1;
-            const classes = isLast ? "py-4" : "py-4 border-b border-gray-300";
- 
-            return (
-              <tr key={id} className="hover:bg-gray-50">
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="font-bold"
-                  >
-                    {id}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    className="font-normal text-gray-600"
-                  >
-                    {date}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    className="font-normal text-gray-600"
-                  >
-                    {status}
-                  </Typography>
-                </td>
-                <td className={classes}>
-                  <Typography
-                    variant="small"
-                    className="font-normal text-gray-600"
-                  >
-                    {price}
-                  </Typography>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </Card>
+          Order History
+          {/* <Button onClick={()=>alert(groupedData)}>Test</Button> */}
         </div>
+        <Card className="h-full w-11/12  px-6 mr-10">
+          <table className="w-full min-w-max table-auto text-left">
+            <thead>
+              <tr>
+                {TABLE_HEAD.map((head) => (
+                  <th key={head} className="border-b border-gray-300 pb-4 pt-10">
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-bold leading-none"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {/* {orders?.map(({ id, date, status, price }, index) => { */}
+              {/* const classes = isLast ? "py-4" : "py-4 border-b border-gray-300"; */}
+              {/* {Object.keys(orders).map((orderId, index) => { */}
+                {orders.map((orderId, index) => {
+                const items = groupedData[orderId];
+                return (
+                  <tr key={index} className="hover:bg-gray-50" onClick={() => handleOpen(orderId.items[0]?.orderId,index)} >
+                    <td className="py-4 border-b border-gray-300">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-bold"
+                      >
+                        {orderId.items[0]?.orderId}  
+                      </Typography>
+                    </td>
+                    <td className="py-4 border-b border-gray-300">
+                      <Typography
+                        variant="small"
+                        className="font-normal text-gray-600"
+                      >
+                        {/* {date} */}
+                      </Typography>
+                    </td>
+                    <td className="py-4 border-b border-gray-300">
+                      <Typography
+                        variant="small"
+                        className="font-normal text-gray-600"
+                      >
+                        {/* {status} */}
+                      </Typography>
+                    </td>
+                    <td className="py-4 border-b border-gray-300">
+                      <Typography
+                        variant="small"
+                        className="font-normal text-gray-600"
+                      >
+                        {/* {price} */}
+                        {orderId.payment}
+                      </Typography>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Card>
+      </div>
+      <Dialog open={open} handler={handleOpen}>
+        <DialogHeader className=' bg-blue-gray-400'>
+          <div className='flex w-full bg-yellow-400 justify-between'>
+            <div className=' bg-red-700 felx flex-col w-1/2'>
+              <div>
+                Order Date
+              </div>
+              <div>
+                Date
+              </div>
+            </div>
+            <div className=' bg-deep-purple-400 flex flex-col w-full'>
+              <div>Price</div>
+              <div>priceeee</div>
+            </div>
+            <div className='bg-red-200 flex '>
+              orderid
+            </div>
+          </div>
+        </DialogHeader>
+        <DialogBody>
+          {selectId}
+          {selectedData.map((item, index) => (
+            <div key={index} className=''>
+              <div className='flex w-full justify-between'>
+              <div>
+                image
+              </div>
+              <div>
+                <p>Product Name</p>
+                <p>Price</p>
+              </div>
+              <div>
+                <p>Quantity : {item.quantity}</p>
+              </div>
+              <div>
+                <Button>Review</Button>
+              </div>
+              {/* divroduct ID: {item.productId}, Quantity: {item.quantity} */}
+            </div>
+
+              </div>
+          ))}
+
+        </DialogBody>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="gray"
+            onClick={handleOpen}
+            className="mr-1"
+          >
+            <span>Close</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
     </div>
+
   )
 }
 
