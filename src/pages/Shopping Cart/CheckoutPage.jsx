812ec29d39f5c9218544f6d7ab3aa73feac476ba
NIMESH939 <nimeshkavindaofficial@gmail.com>
2024-12-02 16:40:08 +0530

    selected data send

--- src/pages/Shopping Cart/CheckoutPage.jsx
+++ src/pages/Shopping Cart/CheckoutPage.jsx
@@ -1,37 +1,23 @@
-import React, { useEffect } from "react";
-import { useCustomContext } from "../../contexts/Context.js";
+import React, { useEffect } from 'react'
+import { useCustomContext } from '../../contexts/Context';
+
+
+
+
 
 const CheckoutPage = () => {
-  // Access selectedItems from Context
-  const { selectedItems, setSelectedItems } = useCustomContext();
+  const { puchaseItems } = useCustomContext();
 
+  
   useEffect(() => {
-    console.log("Selected items in checkout:", selectedItems);
-  }, [selectedItems]);
+    console.log("Items passed to this component:", puchaseItems);
+  }, [puchaseItems]);
 
   return (
-    <div className="min-h-screen bg-gray-100 py-10">
-      <div className="container mx-auto">
-        <h3 className="text-center mb-6 text-xl font-bold">Checkout Page</h3>
-        {/* Display selected items */}
-        <div className="selected-items">
-          {selectedItems.length > 0 ? (
-            <ul>
-              {selectedItems.map((item, index) => (
-                <li key={index}>
-                  <span>
-                    {item.name} - {item.quantity}
-                  </span>
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p>No items selected.</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div>
 
-export default CheckoutPage;
+    </div>
+  )
+}
+
+export default CheckoutPage
